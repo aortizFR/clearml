b4560d1115bf5b877766fe9db9e95415ae4d26ce
aortizFR <ortiz.aurelien@gmail.com>
2025-10-23 10:37:30 +0200

    Fix num_tasks, memory_gb and gpu options

--- gromacs/gromacs_clearml_script.sh
+++ gromacs/gromacs_clearml_script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Apptainer image path
-export APPTAINER_IMAGE="/home/lenovo/clearml/gromacs/gromacs_2023.2.sif"
+export APPTAINER_IMAGE="${HOME}/clearml/gromacs/gromacs_2023.2.sif"
 
 # Gromacs working directory
-export WORKDIR="/home/lenovo/clearml/gromacs/stmv_benchmark/GROMACS_heterogeneous_parallelization_benchmark_info_and_systems_JCP/stmv"
+export WORKDIR="${HOME}/clearml/gromacs/stmv_benchmark/GROMACS_heterogeneous_parallelization_benchmark_info_and_systems_JCP/stmv"
 
 # Gromacs variables
 export GMX_ENABLE_DIRECT_GPU_COMM=1
@@ -13,7 +13,7 @@ apptainer exec --nv ${APPTAINER_IMAGE} nvidia-smi
 
 cd ${WORKDIR}
 apptainer run --nv \
-       --bind /home/lenovo:/home/lenovo:rw \
+       --bind ${HOME}:${HOME}:rw \
        ${APPTAINER_IMAGE} \
        gmx mdrun -ntmpi 8 -ntomp 5 -nb gpu -pme gpu -npme 1 -update gpu -bonded gpu -nsteps 100000 -resetstep 90000 -noconfout -dlb no -nstlist 300 -pin on -v -gpu_id 0
 
--- gromacs/main.py
+++ gromacs/main.py
@@ -8,7 +8,9 @@ from clearml import Task
 task = Task.init()
 logger = task.get_logger()
 
+#----------------------------------------------------------------------------------------------------
 # args for the running task
+#----------------------------------------------------------------------------------------------------
 args = {
     "target_project": "",
     "new_task_name": "",
@@ -23,15 +25,31 @@ args = {
 
 args = task.connect(args)
 
+#----------------------------------------------------------------------------------------------------
 # Load bash script from file
+#----------------------------------------------------------------------------------------------------
 script = Path("gromacs_clearml_script.sh").read_text()
 
+#----------------------------------------------------------------------------------------------------
 # Modify bash script according to values provided from application wizard
+#----------------------------------------------------------------------------------------------------
+# MPI threads
 script = re.sub(r"-ntmpi \d+", f"-ntmpi {args['num_mpi_threads']}", script, 1)
+
+# OpenMP threads
 script = re.sub(r"-ntomp \d+", f"-ntomp {args['num_openmp_threads']}", script, 1)
+
+# GPUs
+gpu_id_str = "".join(str(i) for i in range(args["num_gpus"]))
+script = re.sub(r"-gpu_id \d+", f"-gpu_id {gpu_id_str}", script, 1)
+
+# Working directory
 if args["workdir"]:
     script = re.sub(r"WORKDIR=\"[^\"]*\"", f"WORKDIR={args['workdir']}", script, 1)
 
+#----------------------------------------------------------------------------------------------------
+# Create a ClearML task with the modified bash script
+#----------------------------------------------------------------------------------------------------
 with tempfile.NamedTemporaryFile(mode="w+t", delete=True) as named_temp_file:
 
     # Write back script into temporary file
@@ -46,20 +64,31 @@ with tempfile.NamedTemporaryFile(mode="w+t", delete=True) as named_temp_file:
         binary="/bin/bash",
     )
 
+#----------------------------------------------------------------------------------------------------
+# Update num_tasks according to the number of MPI and OpenMP threads
+#----------------------------------------------------------------------------------------------------
+args["num_tasks"] = args["num_mpi_threads"] * args["num_openmp_threads"]
+
+#----------------------------------------------------------------------------------------------------
 # Update task hyperparameters according to application wizard (these will be used by the Slurm template)
+#----------------------------------------------------------------------------------------------------
 bash_task.set_parameters(
     {
         "properties/num_tasks": args["num_tasks"],
         "properties/num_gpus": args["num_gpus"],
-        "properties/memory_gb": args["num_gpus"],
+        "properties/memory_gb": args["memory_gb"],
     }
 )
 
-# Enqueue the new task to the queue requested in the application wizard (we assume there is a slurm agent
-# monitoring that queue)
+#----------------------------------------------------------------------------------------------------
+# Enqueue the new task to the queue requested in the application wizard
+# (we assume there is a slurm agent monitoring that queue)
+#----------------------------------------------------------------------------------------------------
 Task.enqueue(task=bash_task, queue_name=args["queue_name"])
 
+#----------------------------------------------------------------------------------------------------
 # Monitor the task we just enqueued
+#----------------------------------------------------------------------------------------------------
 status = bash_task.status
 while True:
     print("Monitoring Bash task")
