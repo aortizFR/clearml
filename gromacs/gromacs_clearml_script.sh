9f6cffac80181a06f9f61d68656350b88fc70dc9
Aurelien ORTIZ <ortiz.aurelien@gmail.com>
2025-10-23 09:23:39 +0200

    Gromacs first commit

---
+++ gromacs/gromacs_clearml_script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Apptainer image path
+export APPTAINER_IMAGE="/home/lenovo/clearml/gromacs/gromacs_2023.2.sif"
+
+# Gromacs working directory
+export WORKDIR="/home/lenovo/clearml/gromacs/stmv_benchmark/GROMACS_heterogeneous_parallelization_benchmark_info_and_systems_JCP/stmv"
+
+# Gromacs variables
+export GMX_ENABLE_DIRECT_GPU_COMM=1
+
+apptainer exec --nv ${APPTAINER_IMAGE} nvidia-smi
+
+cd ${WORKDIR}
+apptainer run --nv \
+       --bind /home/lenovo:/home/lenovo:rw \
+       ${APPTAINER_IMAGE} \
+       gmx mdrun -ntmpi 8 -ntomp 5 -nb gpu -pme gpu -npme 1 -update gpu -bonded gpu -nsteps 100000 -resetstep 90000 -noconfout -dlb no -nstlist 300 -pin on -v -gpu_id 0
+
+exit 0
---
+++ gromacs/main.py
@@ -0,0 +1,77 @@
+import re
+import tempfile
+from pathlib import Path
+from time import sleep
+
+from clearml import Task
+
+task = Task.init()
+logger = task.get_logger()
+
+# args for the running task
+args = {
+    "target_project": "",
+    "new_task_name": "",
+    "queue_name": "",
+    "num_mpi_threads": 8,
+    "num_openmp_threads": 5,
+    "num_tasks": 40,
+    "num_gpus": 1,
+    "memory_gb": 8,
+    "workdir": "",
+}
+
+args = task.connect(args)
+
+# Load bash script from file
+script = Path("gromacs_clearml_script.sh").read_text()
+
+# Modify bash script according to values provided from application wizard
+script = re.sub(r"-ntmpi \d+", f"-ntmpi {args['num_mpi_threads']}", script, 1)
+script = re.sub(r"-ntomp \d+", f"-ntomp {args['num_openmp_threads']}", script, 1)
+if args["workdir"]:
+    script = re.sub(r"WORKDIR=\"[^\"]*\"", f"WORKDIR={args['workdir']}", script, 1)
+
+with tempfile.NamedTemporaryFile(mode="w+t", delete=True) as named_temp_file:
+
+    # Write back script into temporary file
+    named_temp_file.write(script)
+    named_temp_file.seek(0)
+
+    # Create a new ClearML task to be managed by this application with the bash script we composed
+    bash_task = Task.create(
+        project_name=args["target_project"],
+        task_name=args["new_task_name"],
+        script=named_temp_file.name,
+        binary="/bin/bash",
+    )
+
+# Update task hyperparameters according to application wizard (these will be used by the Slurm template)
+bash_task.set_parameters(
+    {
+        "properties/num_tasks": args["num_tasks"],
+        "properties/num_gpus": args["num_gpus"],
+        "properties/memory_gb": args["num_gpus"],
+    }
+)
+
+# Enqueue the new task to the queue requested in the application wizard (we assume there is a slurm agent
+# monitoring that queue)
+Task.enqueue(task=bash_task, queue_name=args["queue_name"])
+
+# Monitor the task we just enqueued
+status = bash_task.status
+while True:
+    print("Monitoring Bash task")
+
+    bash_task.reload()
+
+    if bash_task.status != status:
+        print(f"Task changed status to {bash_task.status}")
+        status = bash_task.status
+
+    if status not in ("queued", "in_progress"):
+        print(f"Exiting since task status is {status}")
+        break
+
+    sleep(60)
